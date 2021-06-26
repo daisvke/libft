70ccf9cdb4f57419d82c0e466945149fcd6b92df
alien <daisuketanigawa@live.fr>
2021-06-27 01:20:33 +0200

    Modify names

--- ft_free_array_of_pointers.c
+++ ft_free_array_of_pointers.c
@@ -6,7 +6,7 @@
 /*   By: dtanigaw <dtanigaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 23:23:09 by dtanigaw          #+#    #+#             */
-/*   Updated: 2021/06/27 01:17:37 by dtanigaw         ###   ########.fr       */
+/*   Updated: 2021/06/27 01:20:25 by dtanigaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_free_array_of_pointers(char **array_of_pointers, size_t arr_size)
 	size_t	pointer;
 
 	pointer = 0;
-	while (i < arr_size)
+	while (pointer < arr_size)
 	{
 		free(array_of_pointers[pointer]);
 		++pointer;
