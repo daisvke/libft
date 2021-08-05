c107ec37b4491ae189864b82883b4d0af89e2d40
dtanigaw <dtanigaw@student.42.fr>
2021-08-05 05:58:47 +0200

    Return pointer to str and not int

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: dtanigaw <dtanigaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 21:24:35 by dtanigaw          #+#    #+#             */
-/*   Updated: 2021/05/21 01:12:27 by alien            ###   ########.fr       */
+/*   Updated: 2021/08/05 05:57:52 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,18 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	int	i;
+	char	*pos;
+	int		i;
 
+	pos = NULL;
 	i = ft_strlen(s);
 	while (i >= 0)
 	{
 		if (s[i] == (char)c)
-			return ((char *)s + i);
+		{
+			pos = (char *)s + i;
+			return (pos);
+		}
 		--i;
 	}
 	return (NULL);
