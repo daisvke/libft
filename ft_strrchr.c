659a2166629cbd6ce128da8c8fb73114602ac157
alien <daisuketanigawa@live.fr>
2021-08-05 06:12:16 +0200

    Recover last commit

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: dtanigaw <dtanigaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 21:24:35 by dtanigaw          #+#    #+#             */
-/*   Updated: 2021/08/05 05:57:52 by root             ###   ########.fr       */
+/*   Updated: 2021/08/05 06:11:52 by dtanigaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,13 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	char	*pos;
 	int		i;
 
-	pos = NULL;
 	i = ft_strlen(s);
 	while (i >= 0)
 	{
 		if (s[i] == (char)c)
-		{
-			pos = (char *)s + i;
-			return (pos);
-		}
+			return ((char *)s + i);
 		--i;
 	}
 	return (NULL);
